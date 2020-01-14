4234739457b1c278660404d2a08e1e9216b82cd0
Garima Tyagi <garima408@students.itu.edu>
2020-01-14 12:55:24 -0800

    Counter Aoo

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Hello from './Hello'
 import HelloStateless from './HelloStateless'
 import StateFullBtn from './StateMaintain/StateFullBtn'
-
+import Counter from './Counter/Counter'
 
 function App() {
   return (
     <div className="App">
-      <Hello name="Garima"/>
+      {/* <Hello name="Garima"/>
       <HelloStateless name="Anisha"/>
-      <StateFullBtn />
+      <StateFullBtn /> */}
+      <Counter />
     </div>
   );
 }
